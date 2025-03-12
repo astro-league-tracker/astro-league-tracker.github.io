5e6df7bf3b4f4416ae75c3a4823e545fc96215c7
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2025-03-12 16:32:47 -0600

    point prettier to tailwind configs

--- src/pages/programs/[programId].vue
+++ src/pages/programs/[programId].vue
@@ -16,7 +16,7 @@ const program = computed(() =>
         <h1 class="mb-3.5 text-3xl font-extrabold" v-text="program?.name" />
         <!-- eslint-disable vue/no-v-html -->
         <div
-            class="prose prose-invert prose-slate"
+            class="prose prose-slate prose-invert"
             v-html="micromark(program?.content.replaceAll('\\n', '\n') ?? '')"
         />
         <!-- eslint-enable vue/no-v-html -->
