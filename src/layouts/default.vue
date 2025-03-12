85e49b869f2c673e823355492875f8974a4673dc
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2025-03-12 17:18:30 -0600

    switch up the layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="mx-auto my-24 max-w-2xl text-stone-200">
-        <NuxtLink
-            v-if="$route.path !== '/'"
-            to="/"
-            class="mb-6 block text-sky-300 hover:underline"
+    <div class="min-h-screen min-w-screen bg-slate-950 pt-24 antialiased">
+        <div
+            class="fixed right-0 bottom-[-30vh] left-0 z-10 h-[40vh] bg-linear-45 from-indigo-800 via-purple-800 to-pink-950 blur-[20vh]"
+        />
+        <div
+            class="relative z-20 mx-auto max-w-3xl rounded-xs bg-slate-950/50 px-12 py-10 text-stone-200"
         >
-            &lt;- Back
-        </NuxtLink>
-        <slot />
+            <NuxtLink
+                v-if="$route.path !== '/'"
+                to="/"
+                class="mb-6 block text-sky-300 hover:underline"
+            >
+                &lt;- Back
+            </NuxtLink>
+            <slot />
+        </div>
     </div>
 </template>
